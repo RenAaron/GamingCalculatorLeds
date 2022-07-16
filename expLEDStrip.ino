1710ac7d6044487944ab4034b9434515b4b0d2bd
RenAaron Ellis <73759943+RenAaron@users.noreply.github.com>
2022-07-16 14:18:08 -0400

    Create expLEDStrip.ino

---
+++ expLEDStrip.ino
@@ -0,0 +1,78 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+#define LED_PIN 6
+#define LED_COUNT 8
+#define BRIGHTNESS 5
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
+
+uint32_t colors[13] = {0xff0000, 0xff4000, 0xff9900, 0xffd500, 0x99ff00, 0x3cff00, 0x00ffd5, 0x0084ff, 0x2600ff, 0x8000ff, 0xff00ee, 0xff007b, 0xff003c};
+
+uint32_t ledIndexColors[8] = [0xffffff,0xffffff,0xffffff,0xffffff,0xffffff,0xffffff,0xffffff,0xffffff};
+int state;
+
+int counter = 0;
+
+void bop(int ind, uint32_t c){
+  ledIndexColors[ind] = c;
+  
+}
+
+void updateLEDs(){
+  for(int i = 0; i < 7; i++){
+    if(ledIndexColors[i] < 0xffffff){
+      ledIndexColors[i] += 0x000001;
+    }
+    strip.setPixelColor(i, ledIndexColors[i]);
+  }
+
+  strip.show();
+}
+
+void commandAndFunction(int lis, int whea){
+  if(state == lis){
+    Serial.println("hey");
+    bop(whea, colors[counter]);
+    if(counter > 12){
+      counter = 0;
+    } else{
+      counter++;
+    }
+    state = 9;
+
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+
+      #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(BRIGHTNESS);
+
+}
+
+void loop() {
+
+  if (Serial.available() > 0) {
+    state = Serial.read();
+  }
+
+  updateLEDs();
+
+  commandAndFunction('1',1);
+  commandAndFunction('2',2);
+  commandAndFunction('3',3);
+  commandAndFunction('4',4);
+  commandAndFunction('5',5);
+  commandAndFunction('6',6);
+  commandAndFunction('7',7);
+  commandAndFunction('8',8);
+  
+}   
